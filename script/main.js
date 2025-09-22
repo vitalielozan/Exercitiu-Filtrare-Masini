8420deabd1bf186e699290e28edb6cd045dc7b9c
VitalieLozan <vitalielozan17@outlook.com>
2025-09-22 08:38:48 +0200

    exercised at filter Function

--- script/main.js
+++ script/main.js
@@ -13,31 +13,39 @@ const radioSale = document.querySelectorAll('input[name="Sale"]');
 //   searchWishCar();
 // });
 
-searchCar.addEventListener('input', (e) => {
-  searchWishCar(e.target.value);
+filterForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(filterForm);
+  const objectFormData = Object.fromEntries(formData.entries());
+  const brands = formData.getAll('brand');
+  filterCars(objectFormData, brands);
 });
 
-function searchWishCar(searchItem) {
-  const vehicleListContainer = document.getElementById('vehicle-list');
-  vehicleListContainer.innerHTML = ''; // Stergerea initiala a datelor din container
-  CARS.forEach((car) => {
-    if (!car.model.toLowerCase().includes(searchItem.toLowerCase())) return;
-    if (searchItem) {
-      const carArticle = document.createElement('article');
-      carArticle.innerHTML = `
-      <div class="article"
-        <img src="${car.image}" alt="${
-        car.model
-      }" class="img-article" width="400" height="250" />
-        <h2>${car.brand}</h2>
-        <h3>Model: ${car.model}</h3>
-        <p>Price: <b>$${car.price.toLocaleString()}</b></p>
-        </div>
-      `;
-      vehicleListContainer.appendChild(carArticle);
-    }
-  });
-}
+// searchCar.addEventListener('input', (e) => {
+//   searchWishCar(e.target.value);
+// });
+
+// function searchWishCar(searchItem) {
+//   const vehicleListContainer = document.getElementById('vehicle-list');
+//   vehicleListContainer.innerHTML = ''; // Stergerea initiala a datelor din container
+//   CARS.forEach((car) => {
+//     if (!car.model.toLowerCase().includes(searchItem.toLowerCase())) return;
+//     if (searchItem) {
+//       const carArticle = document.createElement('article');
+//       carArticle.innerHTML = `
+//       <div class="article"
+//         <img src="${car.image}" alt="${
+//         car.model
+//       }" class="img-article" width="400" height="250" />
+//         <h2>${car.brand}</h2>
+//         <h3>Model: ${car.model}</h3>
+//         <p>Price: <b>$${car.price.toLocaleString()}</b></p>
+//         </div>
+//       `;
+//       vehicleListContainer.appendChild(carArticle);
+//     }
+//   });
+// }
 
 // Filtrarea dupa key-ul 'isOnSale'
 // radioForm.addEventListener('submit', (e) => {
@@ -45,34 +53,34 @@ function searchWishCar(searchItem) {
 //   isOnSaleCars();
 // });
 
-radioSale.forEach((radioBtn) => {
-  radioBtn.addEventListener('change', (e) => {
-    isOnSaleCars(e.target.value);
-  });
-});
+// radioSale.forEach((radioBtn) => {
+//   radioBtn.addEventListener('change', (e) => {
+//     isOnSaleCars(e.target.value);
+//   });
+// });
 
-function isOnSaleCars(isOnSale) {
-  const vehicleListContainer = document.getElementById('vehicle-list');
-  vehicleListContainer.innerHTML = ''; // Stergerea initiala a datelor din container
-  const checkedValue = isOnSale ? isOnSale === 'true' : null;
-  console.log(checkedValue);
-  CARS.forEach((car) => {
-    if (checkedValue === null || car.isOnSale === checkedValue) {
-      const carArticle = document.createElement('article');
-      carArticle.innerHTML = `
-      <div class= "article">
-        <img src="${car.image}" alt="${
-        car.model
-      }" class="img-article" width="400" height="250" />
-        <h2>${car.brand}</h2>
-        <h3>Model: ${car.model}</h3>
-        <p>Price: <b>$${car.price.toLocaleString()}</b></p>
-        </div>
-      `;
-      vehicleListContainer.appendChild(carArticle);
-    }
-  });
-}
+// function isOnSaleCars(isOnSale) {
+//   const vehicleListContainer = document.getElementById('vehicle-list');
+//   vehicleListContainer.innerHTML = ''; // Stergerea initiala a datelor din container
+//   const checkedValue = isOnSale ? isOnSale === 'true' : null;
+//   console.log(checkedValue);
+//   CARS.forEach((car) => {
+//     if (checkedValue === null || car.isOnSale === checkedValue) {
+//       const carArticle = document.createElement('article');
+//       carArticle.innerHTML = `
+//       <div class= "article">
+//         <img src="${car.image}" alt="${
+//         car.model
+//       }" class="img-article" width="400" height="250" />
+//         <h2>${car.brand}</h2>
+//         <h3>Model: ${car.model}</h3>
+//         <p>Price: <b>$${car.price.toLocaleString()}</b></p>
+//         </div>
+//       `;
+//       vehicleListContainer.appendChild(carArticle);
+//     }
+//   });
+// }
 
 // Functia pentru randarea masinilor bazata pe parametrul 'pasat'
 function renderCars(carList) {
@@ -99,63 +107,60 @@ function setMaxPriceInSlider() {
   const maxCarPrice = Math.max(...CARS.map((car) => car.price));
   priceSlider.max = maxCarPrice; // Valoarea maxima
   priceSlider.value = maxCarPrice; // Valoarea initiala a silder-ului
+  priceSlider.addEventListener('input', () => {
+    priceValue.textContent = `$${Number(priceSlider.value).toLocaleString()}`;
+    priceValue.classList.add('animate');
+    setTimeout(() => priceValue.classList.remove('animate'), 300);
+  });
   priceValue.textContent = `$${maxCarPrice.toLocaleString()}`;
 }
 
 // Filtrarea masinilor bazata pe alegerile utilizatorului
-function filterCars() {
-  const selectedBrand = Array.from(
-    document.querySelectorAll('input[type="checkbox"]:checked')
-  ).map((checkBox) => checkBox.value);
-
-  console.log(selectedBrand);
-  const maxPrice = parseInt(priceSlider.value);
-  const sortOption = selectOptionSort.value;
-
+function filterCars(formData, brands) {
+  const { price, sale, search, sortOptions } = formData;
+  const maxPrice = parseInt(price);
+  console.log(maxPrice, sale, search, sortOptions, brands);
   let filteredCars = CARS.filter((car) => {
     const matchesBrand =
-      selectedBrand.length === 0 || selectedBrand.includes(car.brand);
+      brands.length === 0 ||
+      brands.some((brand) => brand.toLowerCase() === car.brand.toLowerCase());
     const matchesPrice = car.price <= maxPrice;
-    return matchesBrand && matchesPrice;
+    const matchesSale =
+      sale === undefined || car.isOnSale === (sale === 'true');
+    const matchesSearch =
+      !search || car.model.toLowerCase().includes(search.toLowerCase());
+    return matchesBrand && matchesPrice && matchesSale && matchesSearch;
   });
 
   // Sortarea elementelor bazata pe
-  if (sortOption === 'price-asc') {
-    /* 
-      caz 1 a-b = un numar negativ a o sa fie pozitionat inaintea elementului b
-      caz 2 a-b = 0 ramane ordinea actuala
-      caz 3 a-b = un numar pozitiv a o sa fie pozitionat dupa elementul b
-    */
+  if (sortOptions === 'price-asc') {
     filteredCars.sort((a, b) => a.price - b.price);
-  } else if (sortOption === 'price-desc') {
+  } else if (sortOptions === 'price-desc') {
     filteredCars.sort((a, b) => b.price - a.price);
-  } else if (sortOption === 'alphabetic-asc') {
-    /* 
-     a.model.localeCompare(b.model) returneaza -1, 0, 1
-    */
+  } else if (sortOptions === 'alphabetic-asc') {
     filteredCars.sort((a, b) => a.model.localeCompare(b.model));
-  } else if (sortOption === 'alphabetic-desc') {
+  } else if (sortOptions === 'alphabetic-desc') {
     filteredCars.sort((a, b) => b.model.localeCompare(a.model));
   }
-
+  console.log(filteredCars);
   renderCars(filteredCars);
 }
 
-// Logica pentru a face filtrarea cu price slider
-priceSlider.addEventListener('input', function (event) {
-  priceValue.textContent = `$${parseInt(
-    event.currentTarget.value
-  ).toLocaleString()}`;
-  filterCars();
-});
+// // Logica pentru a face filtrarea cu price slider
+// priceSlider.addEventListener('input', function (event) {
+//   priceValue.textContent = `$${parseInt(
+//     event.currentTarget.value
+//   ).toLocaleString()}`;
+//   filterCars();
+// });
 
-// Logica pentru a face filtrarea cu modelele - input checkbox
-document.querySelectorAll('input[type="checkbox"]').forEach((checkbox) => {
-  checkbox.addEventListener('change', filterCars);
-});
+// // Logica pentru a face filtrarea cu modelele - input checkbox
+// document.querySelectorAll('input[type="checkbox"]').forEach((checkbox) => {
+//   checkbox.addEventListener('change', filterCars);
+// });
 
-// Sorting by options
-selectOptionSort.addEventListener('change', filterCars);
+// // Sorting by options
+// selectOptionSort.addEventListener('change', filterCars);
 
 // Setarea valorii initiale pentru price slider
 setMaxPriceInSlider();
